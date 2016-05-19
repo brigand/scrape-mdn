d840597101865741210198f43d4ff958ae85fd07
Frankie Bagnardi <f.bagnardi@gmail.com>
2016-05-18 22:43:23 -0700

    fixes typo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var parseSearch = require('./parseSearch');
 
 exports.search = (query) => {
   return Promise.resolve().then(() => {
-    return download(query)
+    return download.search(query)
   })
   .then((html) => {
     return parseSearch(html)
