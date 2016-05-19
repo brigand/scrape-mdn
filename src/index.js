6d6917fe8cdb05074bb11d65dd74998417975e33
Frankie Bagnardi <f.bagnardi@gmail.com>
2016-05-18 22:30:21 -0700

    adds primary export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+var download = require('./download');
+var parseSearch = require('./parseSearch');
 
-
+exports.search = (query) => {
+  return Promise.resolve().then(() => {
+    return download(query)
+  })
+  .then((html) => {
+    return parseSearch(html)
+  });
+};
 
